4cbe1b48babac9319c95e9075729e7acf031d22d
Mona <montwor@wp.pl>
2017-09-23 23:46:01 +0200

    Repair counting points

--- game/counting.py
+++ game/counting.py
@@ -1,8 +1,5 @@
-from game.desk import Desk
-
-
-def sortowanie(desk: Desk):
-    tab = [desk.dices[0], desk.dices[1], desk.dices[2], desk.dices[3], desk.dices[[4]]]
+def sortowanie(desk):
+    tab = [desk.dices[0].rank, desk.dices[1].rank, desk.dices[2].rank, desk.dices[3].rank, desk.dices[[4]].rank]
     for number in range(0, 5):
         smallest = tab[0]
         for i in range(number, 5):
@@ -12,63 +9,69 @@ def sortowanie(desk: Desk):
     return tab
 
 
-def count(what: int, desk: Desk, move: int):
+def count(what, desk, move):
     sums = 0
-    if what == 1 or 2 or 3 or 4 or 5 or 6:
+    if what == 1 or what == 2 or what == 3 or what == 4 or what == 5 or what == 6:
         for i in range(1, 6):
-            if desk.dices[i - 1] == what:
-                sums += what
+            if desk.dices[i - 1].rank == what:
+                sums = sums + what
         return sums
     if what == 7:
         pair = 0
         for i in range(1, 5):
             for j in range(i, 6):
-                if desk.dices[i - 1] == desk.dices[j - 1] and desk.dices[i - 1] * 2 > pair:
-                    pair = 2 * desk.dices[i - 1]
-        if move == 1 : return  2*pair
+                if desk.dices[i - 1].rank == desk.dices[j - 1].rank and desk.dices[i - 1].rank * 2 > pair:
+                    pair = 2 * desk.dices[i - 1].rank
+        if move == 1: return 2 * pair
         return pair
     tab = sortowanie(desk)
     if what == 8:
-        if tab[0] == tab [1] and tab[2] == tab[3]:
-            if move == 1: return 2*(tab[0]+tab[1]+tab[2]+tab[3])
-            return tab[0]+tab[1]+tab[2]+tab[3]
+        if tab[0] == tab[1] and tab[2] == tab[3]:
+            if move == 1: return 2 * (tab[0] + tab[1] + tab[2] + tab[3])
+            return tab[0] + tab[1] + tab[2] + tab[3]
         elif tab[0] == tab[1] and tab[3] == tab[4]:
-            if move == 1 : return 2*(tab[0] + tab[1] + tab[3] + tab[4])
+            if move == 1: return 2 * (tab[0] + tab[1] + tab[3] + tab[4])
             return tab[0] + tab[1] + tab[3] + tab[4]
         elif tab[1] == tab[2] and tab[3] == tab[4]:
-            if move == 1: return 2*(tab[1] + tab[2] + tab[3] + tab[4])
+            if move == 1: return 2 * (tab[1] + tab[2] + tab[3] + tab[4])
             return tab[1] + tab[2] + tab[3] + tab[4]
-        else:return 0
+        else:
+            return 0
     if what == 9:
-        if tab[0] == tab [1] == tab[2] or tab[1] == tab[2] == tab[3] or tab[2] == tab[3] == tab[4]:
-            if move == 1:return 6*tab[2]
-            else:return 3*tab[2]
-        else:return 0
+        if tab[0] == tab[1] == tab[2] or tab[1] == tab[2] == tab[3] or tab[2] == tab[3] == tab[4]:
+            if move == 1:
+                return 6 * tab[2]
+            else:
+                return 3 * tab[2]
+        else:
+            return 0
     if what == 10:
-        for num in range (1,5):
-            if tab[num-1]!=num:return 0
+        for num in range(1, 5):
+            if tab[num - 1] != num: return 0
         return 15
     if what == 11:
-        for num in range (2,6):
-            if tab[num-1]!=num:return 0
+        for num in range(2, 6):
+            if tab[num - 1] != num: return 0
         return 20
     if what == 12:
         if tab[0] == tab[1] and tab[3] == tab[4] and (tab[2] == tab[1] or tab[2] == tab[3]):
-            for _ in range (1, 6):
+            for _ in range(1, 6):
                 sums += tab[_]
             return sums
-        else: return 0
+        else:
+            return 0
     if what == 13:
-        if tab[1] == tab[2] == tab [3] and (tab[1] == tab[0] or tab[1] == tab[4]):
-            return 4*tab[1]
-        else:return 0
+        if tab[1] == tab[2] == tab[3] and (tab[1] == tab[0] or tab[1] == tab[4]):
+            return 4 * tab[1]
+        else:
+            return 0
     if what == 14:
         if tab[0] == tab[1] == tab[2] == tab[3] == tab[4]:
-            if move == 1:return 50 + 10*tab[0]
-            return 50 + 5*tab[0]
-        else:return 0
+            if move == 1: return 50 + 10 * tab[0]
+            return 50 + 5 * tab[0]
+        else:
+            return 0
     if what == 15:
         for _ in range(1, 6):
             sums += tab[_]
         return sums
-
--- game/playing.py
+++ game/playing.py
@@ -8,13 +8,14 @@ if __name__ == '__main__':
     for i in range(1, number_of_players + 1):
         game.append(Points())
         name = "Player " + str(i)
-        game[i-1].rename(name)
+        game[i - 1].rename(name)
 
-    for _ in range (1, 16):
-        for i in range (1, number_of_players+1):
+    for _ in range(1, 16):
+        for i in range(1, number_of_players + 1):
+
             # TODO: czyszczenie terminala
-            game[i-1].out()
+            game[i - 1].out()
             moves = Move()
             a, b, c = moves.move()
             score = count(a, b, c)
-            game[i-1].add(a, score)
+            game[i - 1].add(a, score)
